f57f7da6b74136f40bf39e2a7fcb2f1e9f515214
Pedro Santana <pedrosantanait@gmail.com>
2018-07-31 21:53:58 -0400

    Created blocks templates

--- themes/starter/elements/footer_bottom.php
+++ themes/starter/elements/footer_bottom.php
@@ -1,6 +1,20 @@
 <?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 
 
+<?php 
+
+$live = false;
+
+if($live) : ?>
+<!-- production version, optimized for size and speed -->
+
+<!-- <script src="https://cdn.jsdelivr.net/npm/vue"></script> -->
+
+<?php else : ?>
+<!-- development version, includes helpful console warnings -->
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+
+<?php endif; ?> 
 
 <?php View::element('footer_required'); ?>
 
--- themes/starter/home.php
+++ themes/starter/home.php
@@ -2,6 +2,11 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 $this->inc('elements/header.php'); ?>
 <main>
+    <?php
+    $a = new Area('Banner');
+    $a->enableGridContainer();
+    $a->display($c);
+    ?>
     <div class="container">
         <div class="row">
             <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12" id="main">

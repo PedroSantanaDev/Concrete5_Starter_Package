130086b0353064bc4b1daee4a21c82b349f73ad2
Pedro Santana <pedrosantanait@gmail.com>
2018-07-31 22:26:25 -0400

    Created custom templates

---
+++ blocks/content/templates/main/view.php
@@ -0,0 +1,11 @@
+<?php
+    defined('C5_EXECUTE') or die("Access Denied.");
+    $c = Page::getCurrentPage();
+    if (!$content && is_object($c) && $c->isEditMode()) {
+        ?>
+		<div class="ccm-edit-mode-disabled-item"><?=t('Empty Content Block.')?></div> 
+	<?php 
+    } else {
+
+        echo '<div> class="custom-content-block-wrapper"' . $content . '</div>';
+    }
